c9e4b11940c6ff7d393a427f0855bb585e86a323
CarlosAcosta4 <carlosacosta4820@gmail.com>
2023-11-06 17:57:13 -0500

    first commit

---
+++ app/src/main/java/com/example/appsimplerecyclerview/AdapterLista.java
@@ -0,0 +1,54 @@
+package com.example.appsimplerecyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.appsimplerecyclerview.databinding.ItemListBinding;
+
+import java.util.ArrayList;
+
+public class AdapterLista
+        extends RecyclerView.Adapter<AdapterLista.ViewHolder> {
+
+    ArrayList<String> lista = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public AdapterLista.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        return new ViewHolder(
+                ItemListBinding.inflate(
+                        LayoutInflater.from(parent.getContext()),
+                        parent, false)
+        );
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterLista.ViewHolder holder, int position) {
+        String dato = lista.get(position);
+        holder.binding.tvitem.setText(dato);
+    }
+
+    @Override
+    public int getItemCount() { return lista.size();
+    }
+
+    public void setLista(ArrayList<String> data){
+        lista.addAll(data);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ItemListBinding binding;
+
+        public ViewHolder(ItemListBinding itemView){
+            super(itemView.getRoot());
+            binding = itemView;
+        }
+    }
+}
